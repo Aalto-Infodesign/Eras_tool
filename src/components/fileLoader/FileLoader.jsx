00705d95d4e0cd30c48063e0ed4266437beec949
Alessio Brioschi <77493369+Brioche23@users.noreply.github.com>
2026-03-18 13:31:19 +0200

    Test

--- src/components/fileLoader/FileLoader.jsx
+++ src/components/fileLoader/FileLoader.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import LoadDataset from "./FileUpload"
+import LoadDataset from "./LoadDataset"
 import { StateSelection } from "./StateSelection"
 import { ProcessButton } from "./ProcessButton"
 
