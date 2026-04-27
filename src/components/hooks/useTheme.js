4ad5097d34721129e2919ca093736ac3ad1dd67f
Alessio Brioschi <77493369+Brioche23@users.noreply.github.com>
2026-04-27 12:08:04 +0300

    forced dark mode

--- src/components/hooks/useTheme.js
+++ src/components/hooks/useTheme.js
@@ -2,11 +2,12 @@
 import { useState, useEffect } from "react"
 
 export function useTheme() {
-  const [theme, setTheme] = useState(
-    () =>
-      localStorage.getItem("theme") ??
-      (window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark"),
-  )
+  const [theme, setTheme] = useState("dark")
+  // const [theme, setTheme] = useState(
+  //   () =>
+  //     localStorage.getItem("theme") ??
+  //     (window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark"),
+  // )
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme)
