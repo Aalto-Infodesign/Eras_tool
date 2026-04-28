78358d1fc8f6cbdd3cbc2ac0c7c01400dd6d2011
Alessio Brioschi <77493369+Brioche23@users.noreply.github.com>
2026-04-28 10:11:13 +0300

    Startup script corrected by Rigbe

--- eras-tool.sh
+++ eras-tool.sh
@@ -9,7 +9,20 @@ notify-send 'ERAS Tool' 'We are starting your application. This could take up to
 docker pull europe-docker.pkg.dev/finngen-sandbox-v3-containers/eu.gcr.io/eras-tool:latest
 
 # run
-docker run --log-driver syslog --rm -d -p 8080:80 \
+docker run --log-driver syslog --rm -d -p 8566:80 \
   europe-docker.pkg.dev/finngen-sandbox-v3-containers/eu.gcr.io/eras-tool:latest
 
-echo "if the eras tool does not launch automatically, visit: http://localhost:8080"
+# wait until the ERAS Tool is ready
+echo "Waiting for ERAS Tool to be ready, may take few seconds"
+counter=0
+until [ "$(curl -s -o /dev/null -I -w '%{http_code}' "http://localhost:8566")" -eq 200 ]
+do
+    sleep 1
+    ((counter++))
+    echo -ne "Waiting for ERAS Tool to be ready (${counter}s) \r"
+done
+
+# open the ERAS tool in the browser
+firefox --new-tab "http://localhost:8566"
+
+echo "if the eras tool does not launch automatically, visit: http://localhost:8566"
